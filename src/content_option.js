9a1dd4a384db0ba124003b594be65544b1749be2
hemanth <varriyateeshhemanth@gmail.com>
2023-07-16 18:36:09 +0530

    site

---
+++ src/content_option.js
@@ -0,0 +1,171 @@
+const logotext = "Varri Yateesh Hemanth";
+const meta = {
+    title: "varri",
+    description: "I’m Dev",
+};
+
+const introdata = {
+    title: "I’m Varri Yateesh Hemanth",
+    animated: {
+        first: "I am Web Developer.",
+        second: "I code cool websites.",
+        third: "I am also a Devops engineer.",
+    },
+    description: "I am a Webee. I Like to dive into new Technologies . Currently I am working on Web and Devops Technologies",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About my Style of work",
+    aboutme: "Design of the week",
+};
+const worktimeline = [{
+        jobtitle: "First Website",
+        where: "Online",
+        date: "2021",
+    },
+    {
+        jobtitle: "React Websites",
+        where: "Online",
+        date: "2022",
+    },
+    {
+        jobtitle: "Full stack website",
+        where: "Online",
+        date: "2022",
+    },
+    {
+        jobtitle: "Devops",
+        where: "Docker and Jenkins",
+        date: "2023",
+    },
+];
+
+const skills = [{
+        name: "Cpp",
+        value: 90,
+    },
+    {
+        name: "Html && CSS",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Docker",
+        value: 85,
+    },
+    {
+        name: "Devops",
+        value: 75,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "cool looking website that has trending looks. ",
+    },
+    {
+        title: "Devops",
+        description: "Helps you with your automation and to host your website onto AWS.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        description: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "varri",
+    YOUR_FONE: "(91)1234567890",
+    description: "send us your data",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/yateeshhemanth",
+    facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/varri-yateesh-hemanth/",
+    twitter: "https://twitter.com/HemanthVarri",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
